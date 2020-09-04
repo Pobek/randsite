f6a6de739ff5da9f7e738c49f204256b77ae696f
Evyatar Cohen <coevyatar@gmail.com>
2020-09-05 00:13:56 +0300

    Update all records page and add specific record page

--- app/routes.py
+++ app/routes.py
@@ -10,5 +10,13 @@ def index():
 @app.route("/records")
 def records_all():
   page = request.args.get('page', 1, type=int)
-  records = NetflixRecord.query.order_by(NetflixRecord.show_id).paginate(page, 1, error_out=False)
-  return render_template("records.html", title="Netflix Records", records=records)
+  records = NetflixRecord.query.order_by(NetflixRecord.show_id).paginate(page, 3, error_out=False)
+  return render_template("all_records.html", title="Netflix Records", records=records)
+
+@app.route("/record/<record>")
+def record(record):
+  record = NetflixRecord.query.filter_by(show_id=record).first()
+  title = ""
+  if record:
+    title = record.title
+  return render_template("record.html", title=title, record=record)
