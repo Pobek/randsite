4c6cec0c24f0d9f3740242e85bd1e38e5827c57b
Evyatar Cohen <coevyatar@gmail.com>
2020-09-06 23:13:53 +0300

    Add record filter

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,18 @@ from app.models import NetflixRecord
 @app.route("/")
 def index():
   return render_template("index.html")
-
+#TV Show
 @app.route("/records")
 def records_all():
   page = request.args.get('page', 1, type=int)
-  records = NetflixRecord.query.order_by(NetflixRecord.show_id).paginate(page, 3, error_out=False)
-  return render_template("all_records.html", title="Netflix Records", records=records)
+  record_filter = request.args.get('show-type', "All", type=str)
+  records = ""
+  print(record_filter)
+  if record_filter != "All":
+    records = NetflixRecord.query.filter_by(type=record_filter).order_by(NetflixRecord.show_id).paginate(page, 3, error_out=False)
+  else:
+    records = NetflixRecord.query.order_by(NetflixRecord.show_id).paginate(page, 3, error_out=False)
+  return render_template("all_records.html", title="Netflix Records", records=records, record_filter=record_filter)
 
 @app.route("/record/<record>")
 def record(record):
